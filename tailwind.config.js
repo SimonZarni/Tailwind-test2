1496d2605d4e0fa117476f628f71e291dcf0a3f5
SimonZarni <simonallen364@gmail.com>
2024-03-22 11:54:05 +0630

    Added files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      'cutive' :["Original Surfer", "sans-serif"]
+    },
+    colors: {
+      'padauk' :["#fcce00"],
+      'jade' : ["#00a86b"]
+    }
+  },
+  plugins: [
+  ],
+}
